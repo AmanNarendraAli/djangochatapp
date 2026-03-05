f8f6a0666defbbd0a6c4fa9f1fb78745c1fd4449
AmanNarendraAli <amannarendraali@gmail.com>
2026-03-05 16:00:08 +0530

    initial setup done

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    room_name = models.CharField(max_length=255)
+
+    def __str(self):
+        return self.room_name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room,related_name='messages',on_delete=models.CASCADE) #related_name allows us to access messages from a room instance using room.messages
+    sender = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str(self):
+        return f'{self.sender} IN {self.room}: {self.content[:20]}'
